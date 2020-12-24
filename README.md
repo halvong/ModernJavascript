27eefa0b5ee519baa93aedd4bed29680de5e42b8
Hal Vong <halvong@yahoo.com>
2020-12-23 17:42:43 -0800

    add icons and finished Weather App

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-Modern Javascript, Udemy 
+Modern Javascript, Udemy, Shaun Pelling
 12/23/2020, Wed
 
+Finished
 
 #url
-Object Shorthand Notation
 https://www.udemy.com/course/modern-javascript-from-novice-to-ninja/learn/lecture/14241436#overview
 
 #github
 html/css - https://github.com/iamshaunjp/modern-javascript/tree/lesson-100
+https://github.com/iamshaunjp/modern-javascript/tree/lesson-108/weather_app/img
 
 #AccuWeather
 https://developer.accuweather.com/user/me/apps
 
 #commands
 gulp watch
-
--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUI = (data) => {
     const cityDets = data.cityDets;
@@ -16,10 +18,17 @@ const updateUI = (data) => {
                 </div> 
     `;
 
+    //show icons and images
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    let timeSrc = weather.IsDayTime? 'img/day.svg' : 'img/night.svg';
+    time.setAttribute('src', timeSrc);
+
+
     if(card.classList.contains('d-none')) {
         card.classList.remove('d-none');
     }
-
 
 };
 
